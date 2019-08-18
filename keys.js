45caede1c60d0975bd5ffe8d8c30e5cf6fcacd86
Kristina Calumpong <kcalumpong@gmail.com>
2019-08-17 18:42:08 -0700

    added bands in town and spotify

--- keys.js
+++ keys.js
@@ -7,21 +7,3 @@ exports.spotify = {
 
 exports.omdbkey = process.env.OMDB_API_KEY
 
-
-
-//
-// * You should then be able to access your keys information like so
-
-//   ```js
-//   var spotify = new Spotify(keys.spotify);
-//   ```
-
-// 9. Make it so liri.js can take in one of the following commands:
-
-//    * `concert-this`
-
-//    * `spotify-this-song`
-
-//    * `movie-this`
-
-//    * `do-what-it-says`
--- liri.js
+++ liri.js
@@ -1,50 +1,126 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+
+var keys = require("./keys.js").spotify
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: keys.id,
+    //id: keys.spotify.id
+    secret: keys.secret
+});
+
+var fs = require("fs");
 var axios = require("axios");
-var  command = process.argv[2];
-var value = process.argv[3]
+var command = process.argv[2];
+var value = process.argv[3];
 
-switch(command) {
-    
+
+switch (command) {
+
     case 'movie-this':
-        OMDB(value);
-    case 'spotify-this':
-        spotify(value);
-    case 'concert-this':
-        bandsInTown(value);
-    default:
+    if (value === undefined || value === null) {
+        value = 'Mr. Nobody'
+    }
+    OMDB(value);
+    break;
 
+    case 'spotify-this':
+    searchSpotify(value);
+    break;
+
+    case 'concert-this':
+
+    if (value === undefined || value === null) {
+        console.log('Sorry, please enter an artist/band name.');
+        break;
+    }
+    bandsInTown(value);
+    break;
+
+    case "do-what-it-says":
+    doWhatItSays();
+    break;
+
+    default:
+        console.log("error")
 }
 
 function OMDB(movie) {
-    
-    var queryURL = "http://www.omdbapi.com/?s=" + movie + "&apikey=" + keys.omdbkey;
+
+    var queryURL = "http://www.omdbapi.com/?t=" + movie + "&apikey=" + keys.omdbkey;
     console.log(queryURL);
-    
+
     axios.get(queryURL)
-    .then(
-      function(response) {
-          console.log('---------');
-          console.log(response.data.Search[0].Title);
-          console.log(response.data.Search[0].Year);
-          console.log(response.data.Search[0].imdbID);
-          console.log(response.data.Search[0].Poster);
-          console.log('-----------------');
-      })
-      .catch(function(error) {
-        if (error.response) {
-        } else {
-     console.log("Error", error.message);
-        }
-     
-      });
-        
-}
-
-function spotify(song){
-
+        .then(
+            function (response) {
+                console.log('---------');
+                console.log("Title: " + response.data.Title);
+                console.log("Year: " + response.data.Year);
+                console.log("Rating: " + response.data.Rated);
+                console.log("Rotten Tomato Rating: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+                console.log('-----------------');
+            })
+        .catch(function (error) {
+            if (error.response) {
+                console.log("Error", error.message);
+            }
+        });
 }
 
 function bandsInTown(artists) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artists + "/events?app_id=codingbootcamp";
+ 
+    axios.get(queryURL)
 
+        .then(function(response) {
+
+            var concerts = response.data;
+            if (concerts.length == 0 || concerts === undefined) {
+                console.log("-------------")
+                console.log("Sorry this artist has not been found or not on tour")
+                console.log("-------------")
+
+            } else {
+                console.log("-------------")
+                console.log("Venue Name: " + concerts[0].venue.name)
+                console.log("Venue Location: " + concerts[0].venue.city)
+                console.log("Date: " + concerts[0].datetime)
+                console.log("-------------")
+            }
+        })
+
+        .catch(function (error) {
+            if (error.response) {
+                console.log("Error", error.message);
+            }
+        });
+}
+
+function searchSpotify(song) {
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        var spotifyData = data.tracks.items[0];
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else {
+        console.log("Artist's Name: " + spotifyData.album.artists[0].name)
+        console.log("Song Name: " + spotifyData.name)
+        console.log("Album Name: " + spotifyData.album.name)
+        console.log("URL: " + spotifyData.album.external_urls.spotify)
+
+        }
+      
+     
+    });
+}
+
+function doWhatItSays() {
+    fs.readFile('./random.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+        var result = data.split(",")
+        console.log(result[1]);
+        searchSpotify(result[1]);
+    });
 }
